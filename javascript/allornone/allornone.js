33929837873fe91445e9f0070988d0a57417de51
Aayush khandelwal <aayush.khandelwal@vinsol.com>
2012-07-04 10:50:14 +0530

    commit

---
+++ javascript/allornone/allornone.js
@@ -0,0 +1,8 @@
+function checkAll(field) {
+for (var i=0; i < field.length; i++)
+	field[i].checked = true;
+}
+function uncheckAll(field) {
+for (var i = 0; i < field.length; i++)
+	field[i].checked = false;
+}

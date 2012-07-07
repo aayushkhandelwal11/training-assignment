44e20aba723bf7d61126952ba43ab36e0fe22895
Aayush khandelwal <aayush.khandelwal@vinsol.com>
2012-07-07 12:03:08 +0530

    commit

--- javascript/childvisible/childvisible.js
+++ javascript/childvisible/childvisible.js
@@ -1,16 +1,40 @@
 function makevisible(field) {
-document.getElementById("divCheckbox").style.visibility = (field.checked) ? "visible" : "hidden";
-document.getElementById('divCheckbox').focus();
+    document.getElementById("divCheckbox").style.visibility = (field.checked) ? "visible" : "hidden";
+    if(document.getElementById("divCheckbox").style.visibility=="hidden") {
+    var j=document.getElementsByName("color1");
+    for(i = 0; i<j.length;i++) {
+        j[i].checked=false;
+        }
+    } 
+document.getElementById("divCheckbox").focus();
 }
 function makevisible1(field) {
 document.getElementById("divCheckbox1").style.visibility = (field.checked) ? "visible" : "hidden";
-document.getElementById('divCheckbox1').focus();
+   if(document.getElementById("divCheckbox1").style.visibility=="hidden") {
+      var j=document.getElementsByName("muvie1");
+      for(i = 0; i<j.length;i++) {
+         j[i].checked=false;
+         }
+    } 
+document.getElementById("divCheckbox1").focus();
 }
 function makevisible2(field) {
 document.getElementById("divCheckbox2").style.visibility = (field.checked) ? "visible" : "hidden";
-document.getElementById('divCheckbox2').focus();
+   if(document.getElementById("divCheckbox2").style.visibility=="hidden") {
+      var j=document.getElementsByName("bike1");
+      for(i = 0; i<j.length;i++) {
+         j[i].checked=false;
+         }
+    } 
+document.getElementById("divCheckbox2").focus();
 }
 function makevisible3(field) {
 document.getElementById("divCheckbox3").style.visibility = (field.checked) ? "visible" : "hidden";
-document.getElementById('divCheckbox3').focus();
+   if(document.getElementById("divCheckbox3").style.visibility=="hidden") {
+      var j=document.getElementsByName("drink1");
+      for(i = 0; i<j.length;i++) {
+         j[i].checked=false;
+         }
+    } 
+document.getElementById("divCheckbox3").focus();
 }

204d34baa41f9e80d1e0df8d4ed76b15cc573107
Aayush khandelwal <aayush.khandelwal@vinsol.com>
2012-07-07 13:49:33 +0530

    commit

--- javascript/weekdays/weekday.js
+++ javascript/weekdays/weekday.js
@@ -1,30 +1,32 @@
+var checkedcount = 0, str;
 function uncheckAll(field) {
 var i;
 for (i = 0; i < field.length; i++) {
 	field[i].checked = false; }
 }
+
 function calculate(checkgroup) {
 document.getElementById("checkbox2").checked = false;
 var i;
 for (i = 0; i < checkgroup.length; i++) {
 		checkgroup[i].onclick = function () {
-		var checkedcount = 0, str, i ;             
-		document.getElementById("checkbox2").checked = false;
-		for (i = 0; i < checkgroup.length; i++)
-			{
-			
-		if(checkgroup[i].checked)
-		{
-		checkedcount++;
-	  if(checkedcount == 1)
-		str=checkgroup[i].value
-		if(checkedcount == 2)
-		str=str+" , "+checkgroup[i].value
-		if(checkedcount == 3)
-		str=str+" and "+checkgroup[i].value
-		}
+		  var i ;             
+		  document.getElementById("checkbox2").checked = false;
+		  str=null;
+		  checkedcount=0;
+		  for (i = 0; i < checkgroup.length; i++) {
+			    if(checkgroup[i].checked)
+		      {
+		         checkedcount++;
+	           if(checkedcount == 1)
+		         str=checkgroup[i].value
+		         if(checkedcount == 2)
+		         str=str+" , "+checkgroup[i].value
+		         if(checkedcount == 3)
+		         str=str+" and "+checkgroup[i].value
+		       }
 			}
-		if (checkedcount > 3){
+		  if (checkedcount > 3){
 			alert("You can only select a maximum of 3 checkboxes. Already choosen "+str)
 			this.checked=false
 			}

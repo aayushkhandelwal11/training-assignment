f98f30c0362019e136275f6897bed92df4a83fb8
Aayush khandelwal <aayush.khandelwal@vinsol.com>
2012-07-10 12:02:13 +0530

    commit

--- javascript/childvisible/childvisible.js
+++ javascript/childvisible/childvisible.js
@@ -1,40 +1,34 @@
 function makevisible(field) {
     document.getElementById("divCheckbox").style.visibility = (field.checked) ? "visible" : "hidden";
-    if(document.getElementById("divCheckbox").style.visibility=="hidden") {
+    
     var j=document.getElementsByName("color1");
     for(i = 0; i<j.length;i++) {
-        j[i].checked=false;
-        }
-    } 
-document.getElementById("divCheckbox").focus();
+       j[i].checked=field.checked;
+    }
+     
+field.scrollIntoView(true);
 }
 function makevisible1(field) {
 document.getElementById("divCheckbox1").style.visibility = (field.checked) ? "visible" : "hidden";
-   if(document.getElementById("divCheckbox1").style.visibility=="hidden") {
-      var j=document.getElementsByName("muvie1");
-      for(i = 0; i<j.length;i++) {
-         j[i].checked=false;
-         }
-    } 
-document.getElementById("divCheckbox1").focus();
+    var j=document.getElementsByName("muvie1");
+    for(i = 0; i<j.length;i++) {
+        j[i].checked=field.checked;
+    }
+    field.scrollIntoView(true);
 }
 function makevisible2(field) {
 document.getElementById("divCheckbox2").style.visibility = (field.checked) ? "visible" : "hidden";
-   if(document.getElementById("divCheckbox2").style.visibility=="hidden") {
-      var j=document.getElementsByName("bike1");
-      for(i = 0; i<j.length;i++) {
-         j[i].checked=false;
-         }
-    } 
-document.getElementById("divCheckbox2").focus();
+   var j=document.getElementsByName("bike1");
+   for(i = 0; i<j.length;i++) {
+   j[i].checked=field.checked;
+   }
+   field.scrollIntoView(true);
 }
 function makevisible3(field) {
 document.getElementById("divCheckbox3").style.visibility = (field.checked) ? "visible" : "hidden";
-   if(document.getElementById("divCheckbox3").style.visibility=="hidden") {
-      var j=document.getElementsByName("drink1");
-      for(i = 0; i<j.length;i++) {
-         j[i].checked=false;
-         }
-    } 
-document.getElementById("divCheckbox3").focus();
+   var j=document.getElementsByName("drink1");
+   for(i = 0; i<j.length;i++) {
+      j[i].checked=field.checked;
+   }
+field.scrollIntoView(true);
 }

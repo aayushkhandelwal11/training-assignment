3a76b7f17f49fd5d40bee3ce0e6bb4971a8dc499
aayushkhandelwal11 <aayush.khandelwal@vinsol.com>
2012-07-20 17:14:33 +0530

    10thcommit

---
+++ Ruby/exercises/exercise10/array.rb
@@ -0,0 +1,19 @@
+class Array
+  def to_hash
+    @hash =Hash.new(Array.new())
+
+      for item in self
+            @hash[item.to_s.length]+=[item]
+                  
+      end
+      puts @hash
+       
+         result=   @hash.inject({}) do |grouped, (k,v)|
+            r = if k%2==1 then 'odd' else 'even' end
+            grouped[r] = [] if grouped[r].nil?
+            grouped[r] << v
+            grouped
+            end 
+      puts result         
+      end
+end

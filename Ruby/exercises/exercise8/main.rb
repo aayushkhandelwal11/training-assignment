998d851d961034b3108b94094fd25fb170d8a13f
aayushkhandelwal11 <aayush.khandelwal@vinsol.com>
2012-08-09 18:30:42 +0530

    improvement exercise8

--- Ruby/exercises/exercise8/main.rb
+++ Ruby/exercises/exercise8/main.rb
@@ -1,12 +1,15 @@
-require_relative 'power'
+class Array
+  def pow!(number)
+    self.each_with_index.map { |x| x **number }   
+  end
+end  
 arr=[1,6,3,7,89]
 puts arr.to_s
-array=Power.new(arr)
 puts "enter the power"
 answer=gets.chomp
   case answer
   when /^\d+$/
-    puts array.pow!(answer.to_i).to_s
+    puts arr.pow!(answer.to_i).to_s
   else
     puts "Please enter a number or 'quit'"
   end
--- Ruby/exercises/exercise8/power.rb
+++
@@ -1,8 +0,0 @@
-class Power
-  def initialize(arr) 
-    @array=arr
-  end
-  def pow!(number)
-   @array.each_with_index.map { |x| x **number }   
-  end
-end  

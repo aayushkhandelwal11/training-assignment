64f6e21ae22e3ec855ada8eeece54d51a40f30c2
aayushkhandelwal11 <aayush.khandelwal@vinsol.com>
2012-08-10 12:46:42 +0530

    excersise17improvement

--- Ruby/exercises/exercise17/main.rb
+++ Ruby/exercises/exercise17/main.rb
@@ -1,24 +1,19 @@
-def add(time1,time2)
-   array1=time1.split(":").to_a
-   array2=time2.split(":").to_a
-   array1 = array1.collect{|i| i.to_i}
-   array2 = array2.collect{|i| i.to_i}
-   array=[array1,array2].transpose.map {|x| x.reduce(:+)}
-   if array[2]>59 
-      array[2]-=60 
-      array[1]+=1
-   end
-   if array[1]>59 
-      array[1]-=60 
-      array[0]+=1
-   end
-   if array[0]>23
-      puts "1 day "
-      array[0]-=24 
-   end
-   puts array.join(":")
-end
+require 'time'
+def sum_time(time1,time2)
+  temp_time1=Time.parse(time1)
+  temp_time2=Time.parse(time2)
+  seconds = temp_time2.sec + temp_time1.sec
+  minutes = temp_time2.min + temp_time1.min
+  hours = temp_time2.hour + temp_time1.hour
+  days=0
+     if seconds > 60  then minutes+=1 ; seconds=seconds%60 end 
+     if minutes > 60  then hours+=1 ; minutes=minutes%60 end
+     if hours > 24  then days+=1 ; hours=hours%24 end  
 
+  print "#{days} days & " if days > 0
+  puts Time.parse("#{hours}:#{minutes}:#{seconds}").strftime('%T')
+  
+end
 
 
   puts "enter the first time"
@@ -31,5 +26,5 @@ end
   if(time1==nil||time2==nil)
     puts "invalid time"
   else
-    add(time1.to_s,time2.to_s)
+    sum_time(time1.to_s,time2.to_s)
   end

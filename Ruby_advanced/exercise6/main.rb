5c1b2882248da0203f179ed3041a0762f250b8dc
aayushkhandelwal11 <aayush.khandelwal@vinsol.com>
2012-08-13 14:28:12 +0530

    advance_ruby

--- Ruby_advanced/exercise6/main.rb
+++ Ruby_advanced/exercise6/main.rb
@@ -9,32 +9,30 @@ list=nil
 listofobject=[]
 CSV.foreach(file_name) do |row|
   if rowcount==0
+    rowcount=1
     list=row
-  		class_name.module_eval do
-  				 attr_accessor *row
-				 define_method(:initialize) do |*values|
-             row.each_with_index do |varname,i|
-                 instance_variable_set("@"+varname, values[i])
-             end
-         end
-         
-         define_method(:to_s) do
-               s = ""
-      							row.each do |varname|
-         							s << varname + "=" + instance_variable_get("@"+varname).to_s + "  "
-      							end
-      		   	return s
-   			end
-   end 
-  rowcount=1
- 
+    class_name.module_eval do
+      attr_accessor *row
+      define_method(:initialize) do |*values|
+        row.each_with_index do |varname,i|
+          instance_variable_set("@"+varname, values[i])
+        end
+      end
+    define_method(:to_s) do
+      s = ""
+      row.each do |varname|
+        s << varname + "=" + instance_variable_get("@"+varname).to_s + "  "
+      end
+      return s
+    end
+  end 
   else
-     ob = class_name.new  
-     list.each_with_index do |varname,i|
-        ob.instance_variable_set("@"+varname, row[i])
-     end
-  print "You entered: "
-  puts ob.to_s 
-  listofobject<<ob
+    ob = class_name.new  
+    list.each_with_index do |varname,i|
+      ob.instance_variable_set("@"+varname, row[i])
+    end
+    print "You entered: "
+    puts ob.to_s 
+    listofobject<<ob
   end
 end

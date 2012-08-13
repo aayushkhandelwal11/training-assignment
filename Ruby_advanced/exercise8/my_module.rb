d9f39738f27ba0d5ee52217772555014c5a0dd91
aayushkhandelwal11 <aayush.khandelwal@vinsol.com>
2012-08-13 14:05:42 +0530

    advance_ruby

--- Ruby_advanced/exercise8/my_module.rb
+++ Ruby_advanced/exercise8/my_module.rb
@@ -2,29 +2,31 @@ module MyModule
    def chained_aliasing *var
       self.class_eval do 
       		var.each do |meth|
+      		     func=meth
       			   m=self.new
       			   list= m.private_methods-Object.methods
       			   if list.include?(meth)
                       private 
-                   end   
-      			   if  meth.to_s.include?"?"
-      			    k=meth.to_s.chomp('?').to_sym
-      			    old="#{k}_without_logging?"
-      			    newmeth="#{k}_with_logging?"
-      			 else    
-      			 
-      			 	old="#{meth}_without_logging"
-      			    newmeth="#{meth}_with_logging"
-      			 end   
-      	        alias_method old, meth
-               	define_method meth do |*args|
-                	puts "--loging start"
-                	res = send old, *args
-                	puts "--loging end"
-                	
+              end   
+              sign=""
+             	if  meth.to_s.include?"!"
+      			   		sign="!"
+      			   		func= meth.to_s.chop!.to_sym     
+      			   	elsif  meth.to_s.include?"?" 
+      			   	 sign="?"
+      			   	 func= meth.to_s.chop!.to_sym 
+      			   	
+             	end   
+                old="#{func}_without_logging"+sign
+      			      newmeth="#{func}_with_logging"+sign
+      	          alias_method old, meth
+                define_method meth do |*args|
+                  	puts "--loging start"
+                	  res = send old, *args
+                	  puts "--loging end"
                 end
             	alias_method newmeth, meth 
-        	end
-        end 
+      	end
+     end   
     end
 end    
